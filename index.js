e667a02ac23d47ed781901aacd634e6fad08ae50
Mahib <sadmanmahib@gmail.com>
2026-09-21 03:26:48 +0600

    mixed search

--- index.js
+++ index.js
@@ -31,15 +31,23 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
+    const email = req.query.email;
 
-    if(name){
-        const filteredUsers = users.filter(user =>
-            user.name.toLocaleLowerCase().includes(name.toLocaleLowerCase())
-        )
-        return res.json(filteredUsers);
+    let filteredUsers = users;
+
+    if (name) {
+        filteredUsers = filteredUsers.filter(user =>
+            user.name.toLowerCase().includes(name.toLowerCase())
+        );
     }
 
-    res.json(users);
+    if (email) {
+        filteredUsers = filteredUsers.filter(user =>
+            user.email.toLowerCase().includes(email.toLowerCase())
+        );
+    }
+
+    res.json(filteredUsers);
 });
 
 app.get('/users/:id', (req, res) => {
@@ -47,7 +55,7 @@ app.get('/users/:id', (req, res) => {
 
     const user = users.find(user => user.id === id);
 
-    if (!user){
+    if (!user) {
         return res.status(404).json({
             message: "User not found"
         })
@@ -56,7 +64,7 @@ app.get('/users/:id', (req, res) => {
     res.json({ user });
 })
 
-app.get('/search', (req, res) =>{
+app.get('/search', (req, res) => {
     const name = req.query.name;
     const age = req.query.age;
 
@@ -83,9 +91,9 @@ app.put('/users/:id', (req, res) => {
     res.json(user);
 });
 
-app.post ('/users', (req ,res) => {
+app.post('/users', (req, res) => {
     const newUser = req.body;
-    
+
     users.push(newUser);
 
     res.status(201).json(newUser);
@@ -95,8 +103,8 @@ app.delete('/users/:id', (req, res) => {
     const id = parseInt(req.params.id);
 
     const userIndex = users.findIndex(user => user.id === id);
-    
-    if(userIndex === -1){
+
+    if (userIndex === -1) {
         return res.status(404).json({
             message: "User not found"
         })
@@ -105,7 +113,7 @@ app.delete('/users/:id', (req, res) => {
     const deletedUser = users.splice(userIndex, 1);
 
     res.json({
-        message : 'User deleted Successfully',
+        message: 'User deleted Successfully',
         user: deletedUser[0]
     })
 })
